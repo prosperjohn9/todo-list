47564be43b518c71b33d332d6b7017560e59803f
prosperjohn9 <prosperjohn9@gmail.com>
2024-01-30 12:18:27 +0300

    Update styles and dependencies

--- src/todoUI.js
+++ src/todoUI.js
@@ -41,6 +41,13 @@ class TodoUI {
                 this.toggleTodoCompletion(todoId);
             }
         });
+
+        document.querySelectorAll("nav a").forEach((link) => {
+            link.addEventListener("click", function () {
+                document.querySelector("nav a.active")?.classList.remove("active");
+                this.classList.add("active");
+            });
+        });
     }
 
     // Handle filter clicks
